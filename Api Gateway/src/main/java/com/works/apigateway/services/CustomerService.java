f77ab1e2d76a5196b6124eb1c5550a8644244fba
yaseminn <godot85@gmail.com>
2024-12-15 12:44:30 +0300

    Update CustomerService

--- Api Gateway/src/main/java/com/works/apigateway/services/CustomerService.java
+++ Api Gateway/src/main/java/com/works/apigateway/services/CustomerService.java
@@ -1,16 +1,54 @@
 package com.works.apigateway.services;
 
 import com.works.apigateway.entities.Customer;
+import com.works.apigateway.entities.Role;
 import com.works.apigateway.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class CustomerService {
+public class CustomerService implements UserDetailsService {
     final CustomerRepository customerRepository;
     final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Customer> optionalCustomer = customerRepository.findByUsernameEqualsIgnoreCase(username);
+        if (optionalCustomer.isPresent()) {
+            Customer c = optionalCustomer.get();
+            return new User(
+                    c.getUsername(),
+                    c.getPassword(),
+                    c.getEnable(),
+                    true,
+                    true,
+                    true,
+                    parseRole(c.getRoles())
+            );
+        }
+        throw new UsernameNotFoundException("Username Not Found!");
+    }
+    private Collection<? extends GrantedAuthority> parseRole(List<Role> roles) {
+        List<GrantedAuthority> ls = new ArrayList<>();
+        for (Role item : roles) {
+            ls.add( new SimpleGrantedAuthority(item.getName()));
+        }
+        return ls;
+    }
+
     public Customer register(Customer customer) {
         Optional<Customer> optionalCustomer = customerRepository.findByUsernameEqualsIgnoreCase(customer.getUsername());
         if (optionalCustomer.isPresent()) {
